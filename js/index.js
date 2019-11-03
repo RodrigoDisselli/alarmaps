2eac08ef70ecec322f8e4edaa467dbd02f6bb568
Rodrigo Almeida <35853479+RodrigoDisselli@users.noreply.github.com>
2019-11-03 02:03:52 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 window.onload = intro(document.getElementById("text-box"), document.getElementById("col-phone"));
 
 function intro(textBox, phone) {
+
     textBox.classList.add("intro-slide-top");
     phone.classList.add("slide-left");
+
+    setTimeout(()=>{
+        textBox.classList.add("opacity1");
+        phone.classList.add("opacity1");
+    }, "250")
 }
